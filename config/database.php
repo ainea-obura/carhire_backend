bbf0cfaaca1f66665bff43e3a1b8b50f760f16ba
Ainea <ainea@localhost.localdomain>
2022-07-18 16:43:40 +0300

    updatedb

--- config/database.php
+++ config/database.php
@@ -2,6 +2,8 @@
 
 use Illuminate\Support\Str;
 
+$DATABASE_URL=parse_url('DATABASE_URL');
+
 return [
 
     /*
@@ -65,12 +67,17 @@ return [
 
         'pgsql' => [
             'driver' => 'pgsql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            //'url' => env('DATABASE_URL'),
+            //'host' => env('DB_HOST', '127.0.0.1'),
+            'host' => $DATABASE_URL["host"],
+            //'port' => env('DB_PORT', '5432'),
+            'port' => $DATABASE_URL["port"],
+            //'database' => env('DB_DATABASE', 'forge'),
+            'database' => ltrim($DATABASE_URL["path"], "/"),
+            //'username' => env('DB_USERNAME', 'forge'),
+            'username' => $DATABASE_URL["user"],
+            //'password' => env('DB_PASSWORD', ''),
+            'password' => $DATABASE_URL["pass"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
